c45a4e649c3c91891e987baef06c46281df092c2
Artem <art.nindzya.00@mail.ru>
2022-01-31 00:04:18 +0300

    main

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-pass
+from kivy.app import App
+from kivy.uix.widget import Widget
+ 
+ 
+class Pets(Widget):
+     pass
+ 
+ 
+class PetsApp(App):
+    def build(self):
+        return Pets()
+
+
+if __name__ == '__main__':
+     PetsApp().run()
